fc1ca7c88fbc650f9b895ccd1fadda6817a8e51e
saheedmohammed <61905594+saheedmohammed@users.noreply.github.com>
2020-03-07 23:06:41 +0530

    Update api.php

--- src/routes/api.php
+++ src/routes/api.php
@@ -2,7 +2,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 Route::prefix('api')->group(function () {
-Route::group(['middleware' => 'api','namespace' => 'Saheed\AvailabilityCheck\Http\Controllers',], function () {
+Route::group(['middleware' => 'api','namespace' => 'Saheed\AvailabilityCheck\Http\Controllers'], function () {
     Route::get('products', 'ProductController@index')->name('products');
 });
 });
